3c9a1d706232f3d2d38209f0fe3266f817b98d27
dariodasilva <dariotecinfo@gmail.com>
2020-12-13 18:44:49 -0300

    Primeiro projeto com QUARKUS - CRUD

---
+++ src/test/java/com/github/dariodasilva/NativeCrudResoruceIT.java
@@ -0,0 +1,9 @@
+package com.github.dariodasilva;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeCrudResoruceIT extends CrudResoruceTest {
+
+    // Execute the same tests but in native mode.
+}
