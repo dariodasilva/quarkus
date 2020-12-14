0c51213de155f5b88410c73bf6c2f23abb2421ad
dariodasilva <dariotecinfo@gmail.com>
2020-12-13 22:10:09 -0300

    Adição de rotas e persistência em DB Docker

---
+++ src/main/java/com/github/dariodasilva/ProdutoResource.java
@@ -0,0 +1,21 @@
+package com.github.dariodasilva;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("produtos")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class ProdutoResource {
+
+    @GET
+    public List<Produto> buscarTodosProdutos(){
+        return Produto.listAll();
+    }
+
+
+}
