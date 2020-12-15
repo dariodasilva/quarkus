f8f96cf33da41c4f4368a37e1faa24ef2fe12ebf
dariodasilva <dariotecinfo@gmail.com>
2020-12-14 23:05:42 -0300

    Adição de dependência para o Application_json e criação de endpoint POST

---
+++ src/main/java/com/github/dariodasilva/CadastrarProdutoDTO.java
@@ -0,0 +1,12 @@
+package com.github.dariodasilva;
+
+import java.math.BigDecimal;
+
+public class CadastrarProdutoDTO {
+
+    public String nome;
+
+    public BigDecimal valor;
+
+
+}
--- src/main/java/com/github/dariodasilva/ProdutoResource.java
+++ src/main/java/com/github/dariodasilva/ProdutoResource.java
@@ -1,10 +1,9 @@
 package com.github.dariodasilva;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.transaction.Transactional;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import java.util.List;
 
 @Path("produtos")
@@ -16,6 +15,22 @@ public class ProdutoResource {
     public List<Produto> buscarTodosProdutos(){
         return Produto.listAll();
     }
+
+    @POST
+    @Transactional
+    public void buscarTodosProdutos(CadastrarProdutoDTO dto){
+        Produto p = new Produto();
+
+        p.nome = dto.nome;
+        p.valor = dto.valor;
+        p.persist();
+    }
+
+//    @POST
+//    public Response save(Produto produto) {
+//        produto.persist();
+//        return Response.ok(produto).build();
+//    }
 
 
 }
